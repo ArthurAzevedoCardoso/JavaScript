1a568a2c0203691902d9e21e483d5b15c4d17c3a
ArthurAzevedoCardoso <arthurdeazevedocardosodev@gmail.com>
2022-12-04 02:04:55 -0300

    String

---
+++ Tipos de dados/string.js
@@ -0,0 +1,14 @@
+/*String 
+-Cadeia de Caracteres 
+*/
+
+let myFirstString = "E aqui dentro eu pósso escrever o que eu quiser "
+let mySeccondString = 'Agora com apás simples'
+let myThirdString = `Com crase eu tambem consigo ` //Template literals ou template strings 
+
+let numberOfPeopleInClass = 34
+
+let myTextWinthSuperPower = `Tinham exatamente ${numberOfPeopleInClass} pessoas na aula`
+
+console.log(myTextWinthSuperPower)
+
