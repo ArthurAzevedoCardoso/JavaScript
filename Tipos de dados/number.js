c1107ea9b96f8f9f33ef6e846116b3ce611a9e68
ArthurAzevedoCardoso <arthurdeazevedocardosodev@gmail.com>
2022-12-04 02:15:09 -0300

    Numeros

---
+++ Tipos de dados/number.js
@@ -0,0 +1,11 @@
+
+const numberZero = 29
+const numberOne = 15
+let MyfirstMath = `A conta e de multiplicação ${numberZero}*${numberOne}=${numberZero*numberOne}`
+const result = numberZero*numberOne
+
+
+console.log(numberZero)
+console.log(numberOne)
+console.log(MyfirstMath)
+
