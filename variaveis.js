ffa12b6e5f8ce7261edd5b370fdce66fbb57baf4
ArthurAzevedoCardoso <arthurdeazevedocardosodev@gmail.com>
2022-12-04 01:55:59 -0300

    Variaveis

---
+++ variaveis.js
@@ -0,0 +1,25 @@
+// VARIAVEIS
+
+//    Permitidos
+// Iniciar com $ ou_
+// Colocar acento 
+// Iniciar com letras 
+// Letras maisculas e minusculas 
+
+// NÃO PODE 
+// INICIAR COM NUMEROS 
+// COLOCAR COM ESPAÇO 
+
+// IDEAL 
+
+// Canel case 
+// Snake case 
+// SEMPRE em ingles 
+// nomes que fazer sentido 
+//
+
+let number1 = 1 
+const number2 = 2 //este um valor constante
+
+console.log(number1)
+console.log(number2)
