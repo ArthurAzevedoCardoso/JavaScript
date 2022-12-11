af17a4a65cb23a3adc4ad2198c77ea2768aa8f73
ArthurAzevedoCardoso <arthurdeazevedocardosodev@gmail.com>
2022-12-10 21:32:46 -0300

    Operador de comparação

---
+++ Operadores de comparação/operadores.comparação.js
@@ -0,0 +1,18 @@
+/* OPERADORES DE COMPARAÇÃO 
+    == ->Igual//Ele compara o valor, mas não compara o tipo de valor// NUNCA USE ESSE AQUI 
+    === ->TOTALMENTE igual // Ele verifica o tipo do dado 
+
+    != -> Diferente // Ele compara o valor, mas não compara o tipo do valor // NUNCA USE ESSE AQUI 
+    !== -> TOTALMENTE Diferente // Ele verifica o tipo do dado 
+
+*/
+
+
+const firNumber ="23"
+const seccondNumber = 23 
+
+console.log(firNumber == seccondNumber)
+console.log(firNumber === seccondNumber)
+
+console.log(firNumber != seccondNumber)
+console.log(firNumber !== seccondNumber)
