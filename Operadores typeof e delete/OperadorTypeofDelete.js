e6348a8fc237952243f6c5cc60f71c143a1bdcec
ArthurAzevedoCardoso <arthurdeazevedocardosodev@gmail.com>
2022-12-12 00:32:11 -0300

    Operador Typeof and Delete

---
+++ Operadores typeof e delete/OperadorTypeofDelete.js
@@ -0,0 +1,15 @@
+/* OPERADORES TYPE OF E DELETE */
+
+const myNumber = 20 
+const MyString = "Ola, sou uma string"
+
+const myObject ={ 
+    Name: "Object",
+    Age : 20
+} 
+delete  myObject.Age
+
+console.log(typeof myNumber)
+console.log(typeof MyString)
+
+console.log(myObject)
