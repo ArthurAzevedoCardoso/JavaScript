2e7425f52d8a8483e3558c66c5c951b0f1435e7a
ArthurAzevedoCardoso <arthurdeazevedocardosodev@gmail.com>
2022-12-12 00:14:02 -0300

    OPERADORES LOGICOS

---
+++ Operador logicos/OperadorLogico.js
@@ -0,0 +1,19 @@
+/*  OPERADORES LÓGICOS 
+
+    && -> E 
+        TRUE && TRUE = TRUE 
+        TRUE && FALSE = FALSE 
+        FALSE && FALSE = FALSE 
+
+
+    || ->OU 
+
+        TRUE || TRUE = TRUE
+        TRUE || FALSE = TRUE
+        FALSE || FALSE = FALSE
+
+    ! -> NEGAÇÃO 
+        !TRUE = FALSE 
+        !FALSE = TRUE 
+
+*/
