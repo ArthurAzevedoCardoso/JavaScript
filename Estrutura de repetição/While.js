0d5e278bee2cb8e19baadc93b1d4f69b45832c43
ArthurAzevedoCardoso <arthurdeazevedocardosodev@gmail.com>
2022-12-12 21:38:49 -0300

    while

---
+++ Estrutura de repetição/While.js
@@ -0,0 +1,11 @@
+/*ESTRUTURA DE REPETIÇÃO  */
+
+for(let i = 0; i <=4; i++){
+    console.log(i)
+}
+
+const students = ['Bruna', 'Ana', 'João', 'Arthur']
+
+for(let i = 0; i< students.length; i++){
+    console.log(`${students[i]} está presente na aula` )
+}
