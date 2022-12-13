7e72fd1356f55ce20143a282cac1cbf97eff6eef
ArthurAzevedoCardoso <arthurdeazevedocardosodev@gmail.com>
2022-12-12 21:13:27 -0300

    SwitchCase

---
+++ Controle de fluxo/SwitchCase.js
@@ -0,0 +1,33 @@
+/*SWTICH CASE  */
+
+const temperatura = 00
+
+switch (temperatura) {
+    case 30:
+        console.log('A pessoa está com a temperatura MUITO abaixa do normal')
+        break;
+    
+    case 33:
+        console.log('A pessoa está com a temperatura abaixa do normal')
+        break;
+
+    case 36:
+        console.log('A pessoa está com a temperatura normal')
+        break;
+
+    default:
+        console.log('Temperatura não econtrada')
+        break;
+}
+
+const bool =true
+switch (bool) {
+    case true:
+        console.log("it´s true ")
+        break;
+
+        case false:
+            console.log("it´s false ")
+            break;
+
+}
