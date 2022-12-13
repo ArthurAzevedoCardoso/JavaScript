8598b4d2002633bec6334b9bbc92efefdaa6e15c
ArthurAzevedoCardoso <arthurdeazevedocardosodev@gmail.com>
2022-12-12 21:43:52 -0300

    For of

---
+++ Estrutura de repetição/For of.js
@@ -0,0 +1,12 @@
+/* FOR OF  */
+
+let Mymane = 'Arthur'
+let allNames = ['Arthur','Valeria','Alice']
+
+for(let letter of Mymane) {
+    console.log(letter)
+}
+
+for(let name of allNames){
+    console.log(name)
+}
